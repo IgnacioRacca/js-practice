39199a21a808be7097c50112aef31857e55d6f1a
Ignacio J Racca <ijraccat@gmail.com>
2020-10-26 18:07:58 -0300

    excercise 1 js

---
+++ js/excercise1.js
@@ -0,0 +1,22 @@
+// Excercise 1a
+
+var a,b,c;
+a= 5;
+b= 15;
+c= a+b;
+console.log('Excercise 1a. the sum of a+b=c is '+ a + '+' + b + '=' +c)
+
+// Excercise 1b
+
+var string1, strin2, concat;
+
+string1= 'hello';
+string2= ' world';
+concat= string1.concat(string2);
+console.log('Excercise 1b. the concat is: ' +concat);
+
+// Excercise 1c
+
+c= string1.length + string2.length;
+console.log('Excercise 1c. the sum of the lengths of the strings is:' +c);
+
